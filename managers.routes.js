941f84b0f757183570b896df3ecc7ced7cc05ebe
vlada-eng <78151982+vlada-eng@users.noreply.github.com>
2021-10-06 16:55:24 +0200

    init commin

---
+++ managers.routes.js
@@ -0,0 +1,44 @@
+import galleryManagementRoutes from '@/modules/managers/galleryManagement/GalleryManagement.routes.js'
+import configurationManagementRoutes from '@/modules/managers/configurationManagement/ConfigurationManagement.routes.js'
+import domainsManagementRoutes from '@/modules/managers/domainsManagement/DomainsManagement.routes.js'
+import metadataManagementRoutes from '@/modules/managers/metadataManagement/MetadataManagement.routes.js'
+import usersManagementRoutes from '@/modules/managers/usersManagement/UsersManagement.routes.js'
+import profileAttributesRoutes from '@/modules/managers/profileAttributesManagement/ProfileAttributesManagement.routes.js'
+import rolesManagementRoutes from '@/modules/managers/rolesManagement/RolesManagement.routes.js'
+import cacheManagementRoutes from '@/modules/managers/cacheManagement/CacheManagement.routes.js'
+import newsManagementRoutes from '@/modules/managers/newsManagement/NewsManagement.routes.js'
+import templatePruningRoutes from '@/modules/managers/templatePruning/TemplatePruning.routes.js'
+import internationalizationManagementRoutes from '@/modules/managers/internationalizationManagement/InternationalizationManagement.routes.js'
+import mondrianSchemasManagementRoutes from '@/modules/managers/mondrianSchemasManagement/MondrianSchemasManagement.routes.js'
+import dataSourceRoutes from '@/modules/managers/dataSourceManagement/DataSourceManagement.routes.js'
+import functionalitiesManagementRoutes from '@/modules/managers/functionalitiesManagement/FunctionalitiesManagement.routes.js'
+import menuConfigurationRoutes from '@/modules/managers/menuManagement/MenuManagement.routes.js'
+import businessModelCatalogueRoutes from '@/modules/managers/businessModelCatalogue/BusinessModelCatalogue.routes.js'
+import resourceManagementRoutes from '@/modules/managers/resourceManagement/ResourceManagement.routes.js'
+import tenantManagementRoutes from '@/modules/managers/tenantManagement/TenantManagement.routes.js'
+import schedulationAgendaRoutes from '@/modules/managers/schedulationAgendaManagement/SchedulationAgenda.routes.js'
+
+const baseRoutes = []
+
+const routes = baseRoutes
+    .concat(galleryManagementRoutes)
+    .concat(configurationManagementRoutes)
+    .concat(domainsManagementRoutes)
+    .concat(metadataManagementRoutes)
+    .concat(usersManagementRoutes)
+    .concat(profileAttributesRoutes)
+    .concat(rolesManagementRoutes)
+    .concat(cacheManagementRoutes)
+    .concat(newsManagementRoutes)
+    .concat(templatePruningRoutes)
+    .concat(internationalizationManagementRoutes)
+    .concat(mondrianSchemasManagementRoutes)
+    .concat(dataSourceRoutes)
+    .concat(functionalitiesManagementRoutes)
+    .concat(menuConfigurationRoutes)
+    .concat(businessModelCatalogueRoutes)
+    .concat(resourceManagementRoutes)
+    .concat(tenantManagementRoutes)
+    .concat(schedulationAgendaRoutes)
+
+export default routes
